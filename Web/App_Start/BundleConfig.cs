1ccd0e89c8c5d0f0ff16e7d98703fe95180a039c
Seth Petry-Johnson <sjohnson@heuristics.net>
2023-01-28 15:14:03 -0500

    Fixed runtime error from updating jquery

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -4,21 +4,21 @@ namespace Heuristics.TechEval.Web {
 	public class BundleConfig {
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles) {
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+			bundles.Add(new Bundle("~/bundles/jquery").Include(
 						"~/Scripts/jquery-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+			bundles.Add(new Bundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jquery.form").Include(
+			bundles.Add(new Bundle("~/bundles/jquery.form").Include(
 						"~/Scripts/jquery.form.min.js"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+			bundles.Add(new Bundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+			bundles.Add(new Bundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
 
